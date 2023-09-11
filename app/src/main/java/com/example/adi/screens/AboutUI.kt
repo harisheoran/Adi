80dc7bc13d46d3027622426593ae24e6f6ba0e3a
harisheoran <harisheoran01@gmail.com>
2023-09-11 17:21:25 +0530

    added about

--- app/src/main/java/com/example/adi/screens/AboutUI.kt
+++ app/src/main/java/com/example/adi/screens/AboutUI.kt
@@ -2,54 +2,111 @@ package com.example.adi.screens
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
+import com.example.adi.R
+
+data class About(
+    val title: String,
+    val subtitle: String,
+    val icon: Int,
+    val url: String
+)
 
 @Composable
 fun AboutUI(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        GithubCard()
+    val aboutList = listOf<About>(
+        About(
+            title = "Github",
+            subtitle = "Report Bugs, View the Source Code",
+            icon = R.drawable.github,
+            url = "https://github.com/harisheoran/Adi"
+        ),
+        About(
+            title = "Mail",
+            subtitle = "Reach out to us via email",
+            icon = R.drawable.email,
+            url = "mailto:harisheoran01@gmail.com"
+        ),
+        About(
+            title = "TwitterX",
+            subtitle = "Connect with me on twitter",
+            icon = R.drawable.xlogo,
+            url = "https://twitter.com/harisheoran"
+        ),
+
+
+        )
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(1),
+        contentPadding = PaddingValues(
+            start = 18.dp,
+            top = 18.dp,
+            end = 18.dp,
+            bottom = 18.dp
+        ),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(aboutList) {
+            AboutCard(data = it)
+        }
     }
 }
 
 @Composable
-fun GithubCard() {
+fun AboutCard(data: About) {
     val paddingModifier = Modifier.padding(10.dp)
     val context = LocalContext.current
-    val intent = remember { Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/harisheoran/Adi")) }
+    val intent =
+        remember { Intent(Intent.ACTION_VIEW, Uri.parse(data.url)) }
 
-    Card(
-        modifier = paddingModifier
-            .fillMaxWidth()
+    Row(
+        modifier = Modifier
+            .padding(top = 12.dp, start = 12.dp, end = 12.dp, bottom = 12.dp)
             .clickable {
                 startActivity(context, intent, null)
-            }
-    ) {
-        Column() {
-            Text(
-                text = "Github Repository",
-                modifier = paddingModifier,
-                style = MaterialTheme.typography.labelMedium
+            }) {
+        Column(modifier = Modifier.padding(top = 6.dp)) {
+            Image(
+                painter = painterResource(id = data.icon),
+                contentDescription = null,
+                modifier = Modifier.size(25.dp)
             )
+        }
+        Column(modifier = Modifier.padding(start = 16.dp)) {
             Text(
-                text = "harisheoran/Adi",
+                text = data.title,
                 style = MaterialTheme.typography.titleLarge,
-                color = Color.Black,
-                modifier = Modifier.padding(start = 10.dp, end = 10.dp, bottom = 10.dp)
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold
             )
-
+            Text(
+                text = data.subtitle,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W400
+            )
         }
     }
+
 }
